3cb29855fd4ad973e3d6da009d8ffbf9a681f27b
mstopins <mstopins@cisco.com>
2023-10-15 00:57:11 +0200

    initial snippet

---
+++ initial.py
@@ -0,0 +1,26 @@
+
+def localize_objects(path):
+    """Localize objects in the local image.
+
+    Args:
+    path: The path to the local file.
+    """
+    from google.cloud import vision
+
+    client = vision.ImageAnnotatorClient()
+
+    with open(path, "rb") as image_file:
+        content = image_file.read()
+    image = vision.Image(content=content)
+
+    objects = client.object_localization(image=image).localized_object_annotations
+
+    print(f"Number of objects found: {len(objects)}")
+    for object_ in objects:
+        print(f"\n{object_.name} (confidence: {object_.score})")
+        print("Normalized bounding polygon vertices: ")
+        for vertex in object_.bounding_poly.normalized_vertices:
+            print(f" - ({vertex.x}, {vertex.y})")
+
+path = "/Users/mstopins/Desktop/sandbox3/smart_cas.png"
+localize_objects(path)
