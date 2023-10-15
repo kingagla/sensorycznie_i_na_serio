1cb42ff596a4950de22a41290d25820b970a07d9
kinga <kingagla@gmail.com>
2023-10-15 03:34:54 +0200

    add converting and patterns

---
+++ patterns.py
@@ -0,0 +1,9 @@
+
+def hilo(a, b, c):
+    if c < b: b, c = c, b
+    if b < a: a, b = b, a
+    if c < b: b, c = c, b
+    return a + c
+def complement(r, g, b):
+    k = hilo(r, g, b)
+    return tuple(k - u for u in (r, g, b))
