544ef9f8ae8766a15f1625b07fe4bc16f9f046c1
kinga <kingagla@gmail.com>
2023-10-15 03:36:25 +0200

    add converting and patterns update

--- converting.py
+++ converting.py
@@ -1,4 +1,4 @@
-def hex_to_rgb(hex):
+def hex2rgb(hex):
     hex = hex[1:] if hex.startswith("#") else hex
     rgb = []
     for i in (0, 2, 4):
@@ -7,7 +7,7 @@ def hex_to_rgb(hex):
 
     return tuple(rgb)
 
-def rgb_to_hex(r, g, b):
+def rgb2hex(r, g, b):
     return '#{:02x}{:02x}{:02x}'.format(r, g, b)
 
 
